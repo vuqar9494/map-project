1adb61e0b93b3bd38d05a9757686651f65b4f9b5
vuqar9494 <82055193+vuqar9494@users.noreply.github.com>
2023-10-16 09:57:21 +0400

    c

--- src/app/new-item/new-item.component.ts
+++ src/app/new-item/new-item.component.ts
@@ -67,12 +67,15 @@ if (this.isEdit==true) {
     });
   }
   pushData(){
-    this.data.cards.unshift({
-      id: 1,
-      img:  "./assets/Screenshot 2023-10-15 081136.png",
-      subtitle  :  this.addForm.value.title,
-      title :  this.addForm.value.desc,
-      url  :   "/survey/how-many-car-you-have-in-your-family"    })
+    this.dataSouce.forEach(x=>{
+      this.data.cards.unshift({
+        id: 1,
+        img:  "./assets/Screenshot 2023-10-15 081136.png",
+        subtitle  :  x.desc,
+        title :  x.title,
+        url  :   "/survey/how-many-car-you-have-in-your-family"    })
+    })
+ 
 
       this.router.navigate(['/']);
   }
